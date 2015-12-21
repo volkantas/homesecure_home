a9e86bd7f1a2dffbf0f9b1cfac1dc526e5252002
Volkan <volkan@nomadcommerce.com>
2015-12-21 23:44:59 +0200

    sound

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+var config = {
+  serverPort: 3000,
+  homeServerIP: "http://46.101.125.172", //Socket
+  homeServerPort: 3000,
+  ipCamIP: "http://78.182.91.216",
+  ipCamPort: 8090,
+  ipCamUsername: "user",
+  ipCamPassword: "volkamin"
+}
 var express = require('express');
 var app = express();
 var request = require("request");
 var MjpegConsumer = require("mjpeg-consumer");
 var MotionStream = require('./lib/motionstream');
 var FileOnWrite = require("file-on-write");
+var socket = require('socket.io-client')(config.homeServerIP + ":" + config.homeServerPort);
+var SocketIO = require('./lib/socketio.js');
+var Sound = require('./lib/sound.js');
+
+snd = new Sound({
+  socket:socket,
+  path: "Saw_The_Theme.mp3"
+});
+
+//snd.play();
+
+new SocketIO({
+  socket: socket
+});
 
 var writer = new FileOnWrite({
   path: './video',
@@ -17,13 +40,20 @@ var writer = new FileOnWrite({
   sync: true
 });
 
-var consumer = new MjpegConsumer();
-var motion = new MotionStream();
+var streamOptions = {
+  minimumMotion : 2,
+  prebuffer: 0.5,
+  postbuffer: 0.5,
+  minChange: 20
+}
 
-var username = "";
-var password = "";
+var consumer = new MjpegConsumer();
+var motion = new MotionStream(streamOptions);
+
+var username = config.username;
+var password = config.password;
 var options = {
-  url: "http://192.168.1.3/videostream.cgi",
+  url: config.ipCamIP + ":" + config.ipCamPort + "/videostream.cgi",
   headers: {
     'Authorization': 'Basic ' + new Buffer(username + ':' + password).toString('base64')
   }
@@ -31,7 +61,7 @@ var options = {
 
 request(options).pipe(consumer).pipe(motion).pipe(writer);
 
-var server = app.listen(3000, function () {
+var server = app.listen(config.serverPort, function () {
   var host = server.address().address;
   var port = server.address().port;
 
